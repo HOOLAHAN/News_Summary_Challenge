625305e6d19472f298807254bea80327e1b410fa
Iain <iain.hoolahan@gmail.com>
2023-01-25 14:41:24 +0000

    api key added to gitignore, mock data added for guardian api fetch

---
+++ mocks/guardianApiMock.js
@@ -0,0 +1,176 @@
+const guardianApiMock = {
+        "response": {
+            "status": "ok",
+            "userTier": "developer",
+            "total": 2401415,
+            "startIndex": 1,
+            "pageSize": 10,
+            "currentPage": 1,
+            "pages": 240142,
+            "orderBy": "newest",
+            "results": [
+                {
+                    "id": "world/live/2023/jan/25/russia-ukraine-war-live-us-and-germany-expected-to-reveal-tanks-deal-zelenskiy-warns-of-new-wave-of-aggression",
+                    "type": "liveblog",
+                    "sectionId": "world",
+                    "sectionName": "World news",
+                    "webPublicationDate": "2023-01-25T14:00:31Z",
+                    "webTitle": "Russia-Ukraine war live: Zelenskiy ‘sincerely grateful’ for German tank decision as Russia warns it takes conflict ‘to new level’",
+                    "webUrl": "https://www.theguardian.com/world/live/2023/jan/25/russia-ukraine-war-live-us-and-germany-expected-to-reveal-tanks-deal-zelenskiy-warns-of-new-wave-of-aggression",
+                    "apiUrl": "https://content.guardianapis.com/world/live/2023/jan/25/russia-ukraine-war-live-us-and-germany-expected-to-reveal-tanks-deal-zelenskiy-warns-of-new-wave-of-aggression",
+                    "fields": {
+                        "thumbnail": "https://media.guim.co.uk/b63fcd6f57e50d8b87ec766a3f625c881d38a502/0_322_5346_3208/500.jpg"
+                    },
+                    "isHosted": false,
+                    "pillarId": "pillar/news",
+                    "pillarName": "News"
+                },
+                {
+                    "id": "environment/2023/jan/26/unusual-sightings-of-devil-bird-across-melbourne-raise-migration-mysteries-for-researchers",
+                    "type": "article",
+                    "sectionId": "environment",
+                    "sectionName": "Environment",
+                    "webPublicationDate": "2023-01-25T14:00:23Z",
+                    "webTitle": "Unusual sightings of the Asian koel in Melbourne raise mysteries for migration researchers",
+                    "webUrl": "https://www.theguardian.com/environment/2023/jan/26/unusual-sightings-of-devil-bird-across-melbourne-raise-migration-mysteries-for-researchers",
+                    "apiUrl": "https://content.guardianapis.com/environment/2023/jan/26/unusual-sightings-of-devil-bird-across-melbourne-raise-migration-mysteries-for-researchers",
+                    "fields": {
+                        "thumbnail": "https://media.guim.co.uk/ef41e7dbc7cc0a36ba180c8fc278a1365d919920/0_283_5593_3357/500.jpg"
+                    },
+                    "isHosted": false,
+                    "pillarId": "pillar/news",
+                    "pillarName": "News"
+                },
+                {
+                    "id": "film/2023/jan/25/oscars-2023-how-to-watch-almost-every-nominated-feature-doco-and-short-in-australia",
+                    "type": "article",
+                    "sectionId": "film",
+                    "sectionName": "Film",
+                    "webPublicationDate": "2023-01-25T14:00:23Z",
+                    "webTitle": "Oscars 2023: how to watch (almost) every nominated feature, doco and short in Australia",
+                    "webUrl": "https://www.theguardian.com/film/2023/jan/25/oscars-2023-how-to-watch-almost-every-nominated-feature-doco-and-short-in-australia",
+                    "apiUrl": "https://content.guardianapis.com/film/2023/jan/25/oscars-2023-how-to-watch-almost-every-nominated-feature-doco-and-short-in-australia",
+                    "fields": {
+                        "thumbnail": "https://media.guim.co.uk/3f24bc632852315c6581d316ca382d270118987d/0_0_5000_3000/500.jpg"
+                    },
+                    "isHosted": false,
+                    "pillarId": "pillar/arts",
+                    "pillarName": "Arts"
+                },
+                {
+                    "id": "lifeandstyle/2023/jan/25/the-one-change-that-didnt-work-my-radical-decluttering-turned-into-terrible-regret",
+                    "type": "article",
+                    "sectionId": "lifeandstyle",
+                    "sectionName": "Life and style",
+                    "webPublicationDate": "2023-01-25T14:00:23Z",
+                    "webTitle": "The one change that didn’t work: I decluttered my house – then began buying back my belongings",
+                    "webUrl": "https://www.theguardian.com/lifeandstyle/2023/jan/25/the-one-change-that-didnt-work-my-radical-decluttering-turned-into-terrible-regret",
+                    "apiUrl": "https://content.guardianapis.com/lifeandstyle/2023/jan/25/the-one-change-that-didnt-work-my-radical-decluttering-turned-into-terrible-regret",
+                    "fields": {
+                        "thumbnail": "https://media.guim.co.uk/3c8528ce8e8b19db504dc12245a1a1e79c89c6e5/0_448_6720_4032/500.jpg"
+                    },
+                    "isHosted": false,
+                    "pillarId": "pillar/lifestyle",
+                    "pillarName": "Lifestyle"
+                },
+                {
+                    "id": "australia-news/2023/jan/26/unfinished-business-ballarat-abuse-survivor-to-tie-a-ribbon-at-st-marys-before-george-pell-funeral",
+                    "type": "article",
+                    "sectionId": "australia-news",
+                    "sectionName": "Australia news",
+                    "webPublicationDate": "2023-01-25T14:00:23Z",
+                    "webTitle": "‘Unfinished business’: Ballarat abuse survivor to tie a ribbon at St Mary’s before George Pell funeral",
+                    "webUrl": "https://www.theguardian.com/australia-news/2023/jan/26/unfinished-business-ballarat-abuse-survivor-to-tie-a-ribbon-at-st-marys-before-george-pell-funeral",
+                    "apiUrl": "https://content.guardianapis.com/australia-news/2023/jan/26/unfinished-business-ballarat-abuse-survivor-to-tie-a-ribbon-at-st-marys-before-george-pell-funeral",
+                    "fields": {
+                        "thumbnail": "https://media.guim.co.uk/890c41f61d89562c6c5e991363fac54a5d1fa563/527_241_1104_663/500.jpg"
+                    },
+                    "isHosted": false,
+                    "pillarId": "pillar/news",
+                    "pillarName": "News"
+                },
+                {
+                    "id": "food/2023/jan/26/pleat-eat-repeat-three-lunar-new-year-dumpling-recipes-from-rosheen-kaul",
+                    "type": "article",
+                    "sectionId": "food",
+                    "sectionName": "Food",
+                    "webPublicationDate": "2023-01-25T14:00:22Z",
+                    "webTitle": "Pleat, eat, repeat: three lunar new year dumpling recipes from Rosheen Kaul",
+                    "webUrl": "https://www.theguardian.com/food/2023/jan/26/pleat-eat-repeat-three-lunar-new-year-dumpling-recipes-from-rosheen-kaul",
+                    "apiUrl": "https://content.guardianapis.com/food/2023/jan/26/pleat-eat-repeat-three-lunar-new-year-dumpling-recipes-from-rosheen-kaul",
+                    "fields": {
+                        "thumbnail": "https://media.guim.co.uk/6c2c8d22a361aa79a885d1cee5017b0d064c7ce8/0_139_4200_2520/500.jpg"
+                    },
+                    "isHosted": false,
+                    "pillarId": "pillar/lifestyle",
+                    "pillarName": "Lifestyle"
+                },
+                {
+                    "id": "culture/2023/jan/26/ben-stevenson-the-10-funniest-things-i-have-ever-seen-on-the-internet",
+                    "type": "article",
+                    "sectionId": "culture",
+                    "sectionName": "Culture",
+                    "webPublicationDate": "2023-01-25T14:00:22Z",
+                    "webTitle": "Ben Stevenson: the 10 funniest things I have ever seen (on the internet)",
+                    "webUrl": "https://www.theguardian.com/culture/2023/jan/26/ben-stevenson-the-10-funniest-things-i-have-ever-seen-on-the-internet",
+                    "apiUrl": "https://content.guardianapis.com/culture/2023/jan/26/ben-stevenson-the-10-funniest-things-i-have-ever-seen-on-the-internet",
+                    "fields": {
+                        "thumbnail": "https://media.guim.co.uk/09ad08258dff8cf5d0b4ee4c082af9da0795092e/0_628_7383_4429/500.jpg"
+                    },
+                    "isHosted": false,
+                    "pillarId": "pillar/arts",
+                    "pillarName": "Arts"
+                },
+                {
+                    "id": "politics/live/2023/jan/25/nadhim-zahawi-pmqs-tax-hmrc-rishi-sunak-conservatives-uk-politics-latest",
+                    "type": "liveblog",
+                    "sectionId": "politics",
+                    "sectionName": "Politics",
+                    "webPublicationDate": "2023-01-25T13:55:21Z",
+                    "webTitle": "Starmer says Sunak ‘too weak’ to sack Zahawi amid pressure over Tory chair to quit – UK politics live",
+                    "webUrl": "https://www.theguardian.com/politics/live/2023/jan/25/nadhim-zahawi-pmqs-tax-hmrc-rishi-sunak-conservatives-uk-politics-latest",
+                    "apiUrl": "https://content.guardianapis.com/politics/live/2023/jan/25/nadhim-zahawi-pmqs-tax-hmrc-rishi-sunak-conservatives-uk-politics-latest",
+                    "fields": {
+                        "thumbnail": "https://media.guim.co.uk/3bc3c3d8e5f15b9b78f990f3c84354dec903b1d0/0_0_2560_1536/500.jpg"
+                    },
+                    "isHosted": false,
+                    "pillarId": "pillar/news",
+                    "pillarName": "News"
+                },
+                {
+                    "id": "society/2023/jan/25/essex-hospital-withdraws-gas-and-air-for-women-amid-staff-health-concerns",
+                    "type": "article",
+                    "sectionId": "society",
+                    "sectionName": "Society",
+                    "webPublicationDate": "2023-01-25T13:53:28Z",
+                    "webTitle": "Essex hospital pulls gas and air for pregnant women amid concerns for staff",
+                    "webUrl": "https://www.theguardian.com/society/2023/jan/25/essex-hospital-withdraws-gas-and-air-for-women-amid-staff-health-concerns",
+                    "apiUrl": "https://content.guardianapis.com/society/2023/jan/25/essex-hospital-withdraws-gas-and-air-for-women-amid-staff-health-concerns",
+                    "fields": {
+                        "thumbnail": "https://media.guim.co.uk/94fd03cb32db4f11a7ff142fcc5fcd4f771565ee/0_20_3000_1801/500.jpg"
+                    },
+                    "isHosted": false,
+                    "pillarId": "pillar/news",
+                    "pillarName": "News"
+                },
+                {
+                    "id": "uk-news/2023/jan/25/flooding-travel-disruption-forecast-england-cold-temperatures-persist",
+                    "type": "article",
+                    "sectionId": "uk-news",
+                    "sectionName": "UK news",
+                    "webPublicationDate": "2023-01-25T13:51:13Z",
+                    "webTitle": "Flooding and travel disruption for England as cold temperatures persist",
+                    "webUrl": "https://www.theguardian.com/uk-news/2023/jan/25/flooding-travel-disruption-forecast-england-cold-temperatures-persist",
+                    "apiUrl": "https://content.guardianapis.com/uk-news/2023/jan/25/flooding-travel-disruption-forecast-england-cold-temperatures-persist",
+                    "fields": {
+                        "thumbnail": "https://media.guim.co.uk/7dca775edf74fabdc2c552dd65221339662fda97/0_0_4500_2702/500.jpg"
+                    },
+                    "isHosted": false,
+                    "pillarId": "pillar/news",
+                    "pillarName": "News"
+                }
+            ]
+        }
+    }
+
+module.exports = guardianApiMock; 
