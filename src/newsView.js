95d9085735813b7c8cbd457c6fce3fb3b774d1c7
Iain <iain.hoolahan@gmail.com>
2023-01-25 17:38:05 +0000

    display news headline and thumbnail on feed (links to webpage)

--- src/newsView.js
+++ src/newsView.js
@@ -2,7 +2,7 @@ class NewsView {
   constructor(model, client) {
     this.model = model;
     this.client = client;
-    this.mainContainerEl = document.querySelector('#main-container');
+    this.mainEl = document.querySelector('#main');
   }
 
   displayNews() {
@@ -12,14 +12,30 @@ class NewsView {
     
     const news = this.model.getNews();
     const newsFromApi = news.response.results;
+    const storiesEl = document.createElement('h2');
+
+    storiesEl.className = 'stories-header'
+    this.mainEl.append(storiesEl);
 
     const feedEl = document.createElement('div');
     feedEl.className = 'feed';
-    this.mainContainerEl.append(feedEl);
+    this.mainEl.append(feedEl);
 
     newsFromApi.forEach(article => {
       const articleEl = document.createElement('div');
       articleEl.className = 'article';
+      const linkEl = document.createElement('a');
+      linkEl.href = article.webUrl;
+
+      const articleThumbnail = document.createElement('img');
+      articleThumbnail.src = article.fields.thumbnail;
+      
+      const articleTitle = document.createElement('h3');
+      articleTitle.textContent = article.webTitle;
+      
+      linkEl.appendChild(articleThumbnail);
+      linkEl.appendChild(articleTitle);
+      articleEl.append(linkEl);
       feedEl.append(articleEl);
     });
 
